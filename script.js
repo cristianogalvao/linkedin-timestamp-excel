ddd2b618bf5d15fd7dc1baf9340bfeca1075f0f8
Cristiano Galvao <cristianogalvao@users.noreply.github.com>
2022-07-14 22:18:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/** @customfunction */
 function getLinkedInTimestamp(url: string): string {
   const regex = /([0-9]{19})/;
   const rawtimestamp = parseInt(parseInt(regex.exec(url).pop()).toString(2).slice(0, 41),2);
-  const timestamp = new Date(rawtimestamp).toLocaleString().replace(',','');
+  const utc_timestamp = new Date(rawtimestamp).toUTCString();
+  const timestamp = utc_timestamp.substr(5,utc_timestamp.length-5-3-1);
   return timestamp;
 }
